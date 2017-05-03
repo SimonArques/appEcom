673a4fb2d50975588db57e44ec64a1f63e235d28
SimonArques <simonarques@gmail.com>
2017-05-03 10:59:01 +0200

    Changed User Added first sqlite script

--- application/models/User.php
+++ application/models/User.php
@@ -6,13 +6,114 @@
  * Date: 03/05/2017
  * Time: 09:55
  */
-Abstract class Application_Model_User
+class Application_Model_User
 {
 
-    protected $id;
-    protected $name;
-    protected $login;
-    protected $password;
+    private $id;
+    private $name;
+    private $login;
+    private $password;
+    private $isAdmin;
+
+    /**
+     * Application_Model_User constructor.
+     * @param $id
+     * @param $name
+     * @param $login
+     * @param $password
+     * @param $isAdmin
+     */
+    public function __construct($id, $name, $login, $password, $isAdmin)
+    {
+        $this->id = $id;
+        $this->name = $name;
+        $this->login = $login;
+        $this->password = $password;
+        $this->isAdmin = $isAdmin;
+    }
 
     
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getLogin()
+    {
+        return $this->login;
+    }
+
+    /**
+     * @param mixed $login
+     */
+    public function setLogin($login)
+    {
+        $this->login = $login;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * @param mixed $password
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getIsAdmin()
+    {
+        return $this->isAdmin;
+    }
+
+    /**
+     * @param mixed $isAdmin
+     */
+    public function setIsAdmin($isAdmin)
+    {
+        $this->isAdmin = $isAdmin;
+    }
+
+
+
+
 }
